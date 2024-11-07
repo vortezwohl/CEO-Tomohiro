d1f647aead9d570f20f05dca510b969c15b7446b
zihaowu <zihao.wu@igg.com>
2024-11-07 11:50:23 +0800

    指定Name属性

--- tomohiro/tomohiro.py
+++ tomohiro/tomohiro.py
@@ -3,7 +3,10 @@ from langchain_core.language_models import BaseChatModel
 
 from tomohiro.ability import *
 
+NAME = 'Tomohiro'
+
+
 class Tomohiro(Agent):
     def __init__(self, brain: BaseChatModel):
         abilities = [calculator, get_current_datetime, search_internet]
-        super().__init__(abilities=abilities, brain=brain)
+        super().__init__(abilities=abilities, brain=brain, name=NAME)
